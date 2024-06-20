03ae889d986ab725dd265c1de58c1cefc53afea4
ChaRob <how8160@naver.com>
2024-06-21 01:00:21 +0900

    [Tetris] GameManager의 생성자 소멸자 수정, 다른 Manager의  실행 기반 구현

--- Tetris/Managers/GameManager.cpp
+++ Tetris/Managers/GameManager.cpp
@@ -1,26 +1,29 @@
 #include "GameManager.h"
 
-GameManager* GameManager::instance = nullptr;
+GameManager GameManager::instance;
 
 GameManager::GameManager() :
 	m_hDC(0),
 	m_hwnd(0),
 	m_ptResolution(POINT())
-{
-}
+{}
 
 GameManager::~GameManager()
 {
+	// Window API를 이용하여 메인 윈도우 핸들과 메인 윈도우 DC의 메모리를 해제합니다.
+	ReleaseDC(m_hwnd, m_hDC);
 }
 
 int GameManager::InitManager(HWND _hwnd, POINT _ptResolution)
 {
-	// 전역 윈도우 핸들을 GameManager가 갖고 있도록 함.
+	// 전역 윈도우 핸들을 GameManager가 갖고 있도록 합니다.
 	m_hwnd = _hwnd;
 
-	// 해상도에 맞도록 윈도우 크기를 조절한다.
+	// 해상도에 맞도록 윈도우 크기를 조절합니다.
 	m_ptResolution = _ptResolution;
 	ChangeWindowSize(m_ptResolution);
+
+	// Manager들의 초기화 함수를 이곳에 작성합니다.
 
 	return S_OK;
 }
@@ -37,5 +40,6 @@ void GameManager::ChangeWindowSize(Vector2 _resolution)
 
 void GameManager::Update()
 {
+	// Manager들의 Update문을 이곳에 작성합니다.
 
 }
--- Tetris/Managers/GameManager.h
+++ Tetris/Managers/GameManager.h
@@ -10,13 +10,10 @@ class GameManager
 private:
 	GameManager();
 	~GameManager();
-	static GameManager* instance;
+	static GameManager instance;
 public:
 	static GameManager* GetInstance() {
-		if (instance == nullptr) {
-			instance = new GameManager();
-		}
-		return instance;
+		return &instance;
 	}
 private:
 	HWND m_hwnd;			// 메인 윈도우 핸들
@@ -30,6 +27,10 @@ public:
 	
 	// 각 Manager가 루프를 돌 동안 실행할 코드를 이곳에 정리합니다.
 	void Update();
+
+	// 외부에서 필요한 속성값을 전달하는 함수를 여기에 모아뒀습니다.
+	inline HWND GetMainHwnd() { return m_hwnd; }
+	inline HDC GetMainhDC() { return m_hDC; }
 };
 
 #endif // !__DEF_GAME_MANAGER__
