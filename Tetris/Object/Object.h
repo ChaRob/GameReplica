fab1551af5d796f2ccc1c81f4765b96f303a06ca
ChaRob <how8160@naver.com>
2024-06-25 00:50:19 +0900

    [Tetris] Object에 size 추가

--- Tetris/Object/Object.h
+++ Tetris/Object/Object.h
@@ -13,11 +13,14 @@ public:
 private:
 	string m_name;
 	Vector2 m_pos;
+	Vector2 m_size;
 
 public:
 	Vector2 GetPos() const { return m_pos; }
 	void SetPos(const Vector2& _vec) { m_pos = _vec; }
 	string GetName() const { return m_name; }
 	void SetName(const string& _name) { m_name = _name; }
+	Vector2 GetSize() const { return m_size; }
+	void SetSize(const Vector2& _vec) { m_size = _vec; }
 };
 
